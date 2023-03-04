df1e9905514ac463ee43770318b28899e1b00a20
Sami Yousef <mail@samiyousef.ca>
2023-03-03 21:47:01 -0500

    Drop refresh token database on `migrate down`

--- migration/src/m20220101_000001_create_table.rs
+++ migration/src/m20220101_000001_create_table.rs
@@ -76,6 +76,14 @@ impl MigrationTrait for Migration {
 	async fn down(&self, manager: &SchemaManager) -> Result<(), DbErr> {
 		manager
 			.drop_table(Table::drop().table(User::Table).if_exists().to_owned())
+			.await?;
+		manager
+			.drop_table(
+				Table::drop()
+					.table(RefreshTokenEntry::Table)
+					.if_exists()
+					.to_owned(),
+			)
 			.await
 	}
 }
