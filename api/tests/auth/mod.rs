737bb01a023656e53d589a40bc94a198d4cb7aa6
Sami Yousef <mail@samiyousef.ca>
2023-03-08 00:24:10 -0500

    Move`create_app` add to testing root

--- api/tests/auth/mod.rs
+++ api/tests/auth/mod.rs
@@ -1,1 +1,61 @@
+use actix_http::Request;
+use actix_web::{dev::ServiceResponse, web::{self, Data}, App};
+use api::{JsonError, Argon2Config, Config, AppState};
+use hmac::{Hmac, Mac};
+use migration::{Migrator, MigratorTrait};
+use uaparser::UserAgentParser;
+use actix_web::test;
+use actix_service::Service;
+
+
 mod create_user;
+mod magic_link;
+
+pub async fn create_app() -> impl Service<Request, Response = ServiceResponse, Error = actix_web::Error> {
+	// Create a new HMAC-SHA256 key
+	let secret_key = Hmac::new_from_slice("a_secret_key".repeat(3).as_bytes()).unwrap();
+
+	// Load the regexes from the YAML file
+	let ua_parser = UserAgentParser::from_yaml("../regexes.yaml").unwrap();
+
+	// Create a connection to the database
+	let connection = sea_orm::Database::connect("sqlite://../test.sqlite?mode=rwc".to_string())
+		.await
+		.unwrap();
+
+	// Run the migrations
+	Migrator::up(&connection, None).await.unwrap();
+
+	// Create the JSON config
+	let json_cfg = web::JsonConfig::default().error_handler(|err, _req| {
+		let err = format!("Error parsing JSON: {err}");
+		log::warn!("{err}");
+		JsonError {
+			message: err,
+			error_code: "JSON_ERROR".to_string(),
+		}
+		.into()
+	});
+
+	test::init_service(
+		App::new()
+			.app_data(Data::new(AppState {
+				config: Config {
+					bind_addr: "not_used".to_string(),
+					connection_url: "sqlite://../test.sqlite?mode=rwc".to_string(),
+					base_url: "http://turbocore".to_string(),
+					secret_key,
+					debug_level: "debug".to_string(),
+					argon2_config: Argon2Config::default(),
+					minimum_password_strength: 1,
+					mailer: None,
+					email: None,
+				},
+				connection,
+				ua_parser,
+			}))
+			.app_data(json_cfg)
+			.configure(api::auth::add_routes),
+	)
+	.await
+}
