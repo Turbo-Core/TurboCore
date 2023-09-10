6d5bf33a58fa48baefd1e12087b31ab853389417
Sami Yousef <mail@samiyousef.ca>
2023-09-09 20:16:29 -0400

    Add Cargo.toml to entity module

--- api/src/admin/mod.rs
+++ api/src/admin/mod.rs
@@ -1,9 +1,11 @@
 use actix_web::web;
 
 pub mod create_admin;
+pub mod login;
 pub mod middleware;
 
 
 pub fn add_routes(cfg: &mut web::ServiceConfig) {
-    cfg.service(crate::admin::create_admin::handler);
+    cfg.service(crate::admin::create_admin::handler)
+        .service(crate::admin::login::handler);
 }
